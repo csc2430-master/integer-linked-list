0f60d06d96a329c4cdcbaa332868643e535c5675
Carlos Roberto Arias Arevalo <arias@spu.edu>
2022-05-27 09:33:16 -0700

    LinkedList declaration done

---
+++ linked_list.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Carlos R. Arias on 5/27/22.
+//
+
+#include "linked_list.h"
---
+++ linked_list.h
@@ -0,0 +1,36 @@
+//
+// Created by Carlos R. Arias on 5/27/22.
+//
+
+#ifndef INTEGER_LINKED_LIST_LINKED_LIST_H
+#define INTEGER_LINKED_LIST_LINKED_LIST_H
+
+#include <string>
+using std::string;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+class LinkedList {
+private:
+    Node* _head;
+    size_t _size;
+public:
+    LinkedList();
+    ~LinkedList();
+    LinkedList(const LinkedList& ll);
+    const LinkedList& operator=(const LinkedList& rhs);
+    bool Insert(int element, size_t position);
+    int Remove(size_t position);
+    int Get(size_t position)const;
+    int IndexOf(int element)const;
+    void Clear();
+    bool IsEmpty()const;
+    size_t Size();
+    string ToJSON()const;
+};
+
+
+#endif //INTEGER_LINKED_LIST_LINKED_LIST_H
